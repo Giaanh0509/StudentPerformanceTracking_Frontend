da869abc71ea035a254b0edfa7ed2e7dd898ec23
Giaanh0509 <ngotthe0509@gmail.com>
2024-11-11 22:39:45 +0700

    set up tailwind-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './App.css';
+import { Dashboard } from './components/shared/Dashborad';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Dashboard></Dashboard>
   );
 }
 
